bd062db3ae7910858c97cdbf42ef6b7c6dc91f69
aisha <aishaidir94@gmail.com>
2019-01-21 14:48:16 +0100

    initial commit

---
+++ testfile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+class testfile extends Component {
+  render() {
+    return (
+      <div>
+        <h1>first git project</h1>
+      </div>
+    )
+  }
+}
+
+export default testfile
